643239e82837ca4fedba1ef36690016be64f9de8
Trushal Pal <129409515+Trushal-Talhah@users.noreply.github.com>
2026-08-27 21:10:52 +0530

    Add crash reporter to Vercel entrypoint

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,17 @@
-const app = require('../src/app');
+let app;
+try {
+  app = require('../src/app');
+} catch (error) {
+  const express = require('express');
+  app = express();
+  app.all('*', (req, res) => {
+    res.status(500).json({
+      error: 'SERVER_STARTUP_CRASH',
+      message: error.message,
+      stack: error.stack
+    });
+  });
+}
 
 // Export the Express app for Vercel Serverless Functions
 module.exports = app;
